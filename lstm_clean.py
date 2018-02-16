c4d63fcac56901f1b1d6c04cf09d688b0b4d3274
itachi-rg <rohan.gangaraju@gmail.com>
2018-02-16 01:44:26 -0500

    Cleaned code

--- lstm_clean.py
+++ lstm_clean.py
@@ -9,174 +9,113 @@ from tensorflow.contrib import rnn
 import matplotlib.pyplot as plt
 
 
-iterations=10000
-#unrolled through these many time steps
-time_steps=50
+def train(training_file, iterations=5000, time_steps=50, num_lstm_hidden_units=128, num_features=1, num_classes=1, batch_size=128, learning_rate=0.001, split_percent=0.8, print_iter=100):
 
-#hidden LSTM units
-num_units=128
-
-# number of features - for now time series only 1
-n_input=1
-
-#learning rate for adam
-learning_rate=0.001
-
-# number of classes - for now only one real value 
-n_classes=1
-
-#size of batch for each iteration
-batch_size=128
-
-
-# In[3]:
-
-# Read data appropriately
-
-#series = read_csv('shampoo.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-
-series = read_csv('/home/rgangaraju/chaos/entropy_data/xalan-smallJikesRVM-both-100k-64.entropy')
-
-# In[4]:
-
-# Convert series to dataframe to perform shift and generate test set
-series_dataframe = DataFrame(series)
-shifted_dataframe = series_dataframe.shift(1)
-shifted_dataframe.fillna(0, inplace=True)
-
-
-series_dataframe_narray =  series_dataframe.as_matrix()
-shifted_dataframe_narray = shifted_dataframe.as_matrix()
-
-split_length = time_steps
-num_samples = shifted_dataframe_narray.shape[0]-split_length
-
-X_samples = np.zeros([num_samples,split_length])
-y_samples = np.zeros([num_samples,1])
-
-for i in range(num_samples) :
-    X_samples[i] = shifted_dataframe_narray[i:i+split_length,0]
-    y_samples[i] = series_dataframe_narray[i+split_length,0]
-
-X_samples = np.reshape(X_samples,[X_samples.shape[0],X_samples.shape[1],1])
-#print "Input samples shape ", X_samples.shape
-#print "Output samples shape", y_samples.shape
-#print y_samples
-
-
-# In[5]:
-
-split_percent = 0.8
-split = int(split_percent*num_samples)
-X_train_samples = X_samples[:split]
-y_train_samples = y_samples[:split]
-X_test_samples = X_samples[split:]
-y_test_samples = y_samples[split:]
-
-#print "X_train_samples shape", X_train_samples.shape
-#print "X_test_samples shape", X_test_samples.shape
-
-
-# In[6]:
-
-# Create static graph
-
-out_weights=tf.Variable(tf.random_normal([num_units,n_classes]))
-out_bias=tf.Variable(tf.random_normal([n_classes]))
-
-#input image placeholder
-x=tf.placeholder("float",[None,time_steps,n_input])
-#input label placeholder
-y=tf.placeholder("float",[None,n_classes])
-
-#processing the input tensor from [batch_size,n_steps,n_input] to "time_steps" number of [batch_size,n_input] tensors
-tfinput=tf.unstack(x ,time_steps,1)
-
-#defining the network
-lstm_layer=rnn.BasicLSTMCell(num_units,forget_bias=1)
-outputs,_=rnn.static_rnn(lstm_layer,tfinput,dtype="float32")
-
-#converting last output of dimension [batch_size,num_units] to [batch_size,n_classes] by out_weight multiplication
-prediction=tf.matmul(outputs[-1],out_weights)+out_bias
-#loss_function
-#loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y))
-#loss = tf.reduce_mean(tf.pow(prediction - y,2))
-
-loss_array = tf.pow(prediction-y,2)
-
-loss = tf.reduce_mean(loss_array)
-
-tf_mean_loss, tf_loss_variance = tf.nn.moments(loss_array,axes=[0])
-#optimization
-opt=tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-
-#model evaluation
-#correct_prediction=tf.equal(tf.argmax(prediction,1),tf.argmax(y,1))
-#accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-#accuracy = tf.reduce_mean(tf.pow(prediction - y,2))
-
-
-# In[27]:
-saver = tf.train.Saver()
-
-init=tf.global_variables_initializer()
-with tf.Session() as sess:
-    sess.run(init)
-    latest_cp = tf.train.latest_checkpoint('/home/rgangaraju/lstm/cp1')
-    if latest_cp is not None:
-        print("Restoring from ", latest_cp)
-        saver.restore(sess, latest_cp)
-    iter=1
-    while iter<iterations:
-        #batch_x,batch_y=mnist.train.next_batch(batch_size=batch_size)
-
-        #batch_x=batch_x.reshape((batch_size,time_steps,n_input))
-
-        sess.run(opt, feed_dict={x: X_train_samples, y: y_train_samples})
-
-        if iter %10==0:
-            #acc=sess.run(accuracy,feed_dict={x: X_train_samples, y: y_train_samples})
-            mean_loss,loss_variance=sess.run([tf_mean_loss,tf_loss_variance],feed_dict={x: X_train_samples, y: y_train_samples})
-            print(iter, " : iteration , Mean Loss :", mean_loss, "Variance :", loss_variance, " MSE/Var :", (mean_loss/loss_variance) )
-            
-            saver.save(sess, "/home/rgangaraju/lstm/cp1/model.ckpt", global_step = iter)
-
-        iter=iter+1
+    series = read_csv(training_file)
     
-    train_preds, train_loss, train_variance=sess.run([prediction,tf_mean_loss,tf_loss_variance],feed_dict={x: X_train_samples, y: y_train_samples})
-    test_preds, test_loss, test_variance=sess.run([prediction,tf_mean_loss,tf_loss_variance],feed_dict={x: X_test_samples, y: y_test_samples})
-    #print train_preds
-    #print test_preds
+    # Convert series to dataframe to perform shift and generate test set
+    series_dataframe = DataFrame(series)
+    shifted_dataframe = series_dataframe.shift(1)
+    shifted_dataframe.fillna(0, inplace=True)
+
+    # DataFrames to Numpy arrays
+    series_dataframe_narray =  series_dataframe.as_matrix()
+    shifted_dataframe_narray = shifted_dataframe.as_matrix()
+
+    # Create small samples of time_steps size
+    split_length = time_steps
+    num_samples = shifted_dataframe_narray.shape[0]-split_length
+
+    X_samples = np.zeros([num_samples,split_length])
+    y_samples = np.zeros([num_samples,1])
+
+    for i in range(num_samples) :
+        X_samples[i] = shifted_dataframe_narray[i:i+split_length,0]
+        y_samples[i] = series_dataframe_narray[i+split_length,0]
+
+    # Add additional dimension to feed it into tensorflow. It needs 3D tensor for LSTM
+    X_samples = np.reshape(X_samples,[X_samples.shape[0],X_samples.shape[1],1])
+
+
+    # Split into training and test data
+    split = int(split_percent*num_samples)
+    X_train_samples = X_samples[:split]
+    y_train_samples = y_samples[:split]
+    X_test_samples = X_samples[split:]
+    y_test_samples = y_samples[split:]
+
+    # Create static graph
+
+    out_weights=tf.Variable(tf.random_normal([num_lstm_hidden_units,n_classes]))
+    out_bias=tf.Variable(tf.random_normal([n_classes]))
+
+    #input image placeholder
+    x=tf.placeholder("float",[None,time_steps,num_features])
+    #input label placeholder
+    y=tf.placeholder("float",[None,n_classes])
+
+    #processing the input tensor from [batch_size,n_steps,num_features] to "time_steps" number of [batch_size,num_features] tensors
+    tfinput=tf.unstack(x ,time_steps,1)
+
+    #defining the network
+    lstm_layer=rnn.BasicLSTMCell(num_lstm_hidden_units,forget_bias=1)
+    outputs,_=rnn.static_rnn(lstm_layer,tfinput,dtype="float32")
+
+    #converting last output of dimension [batch_size,num_lstm_hidden_units] to [batch_size,n_classes] by out_weight multiplication
+    prediction=tf.matmul(outputs[-1],out_weights)+out_bias
     
+    loss_array = tf.pow(prediction-y,2)
 
+    loss = tf.reduce_mean(loss_array)
+
+    tf_mean_loss, tf_loss_variance = tf.nn.moments(loss_array,axes=[0])
     
+    #optimization
+    opt=tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
 
-# In[28]:
+    saver = tf.train.Saver()
 
-#print y_test_samples, train_preds, test_preds, 
+    # Run created graph
+
+    init=tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        latest_cp = tf.train.latest_checkpoint('/home/rgangaraju/lstm/cp1')
+        if latest_cp is not None:
+            print("Restoring from ", latest_cp)
+            saver.restore(sess, latest_cp)
+        iter=1
+        while iter<iterations:
+            sess.run(opt, feed_dict={x: X_train_samples, y: y_train_samples})
+
+            if iter%print_iter==0:
+
+                mean_loss, loss_variance=sess.run([tf_mean_loss,tf_loss_variance],feed_dict={x: X_train_samples, y: y_train_samples})
+                print(iter, " : iteration , Mean Loss :", mean_loss, "Variance :", loss_variance, " MSE/Var :", (mean_loss/loss_variance) )
+                
+                saver.save(sess, "/home/rgangaraju/lstm/cp1/model.ckpt", global_step = iter)
+
+            iter=iter+1
+        
+        # Get the predictions from the model to construct the graph
+        train_preds, train_loss, train_variance=sess.run([prediction,tf_mean_loss,tf_loss_variance],feed_dict={x: X_train_samples, y: y_train_samples})
+        test_preds, test_loss, test_variance=sess.run([prediction,tf_mean_loss,tf_loss_variance],feed_dict={x: X_test_samples, y: y_test_samples})
 
 
-# In[29]:
+    test_plot = np.concatenate([X_train_samples[0], train_preds, test_preds])
 
-#print train_preds.shape, test_preds.shape
-#print X_train_samples[0].shape
-#print shifted_dataframe_narray.shape
-test_plot = np.concatenate([X_train_samples[0], train_preds, test_preds])
+    print("Final test loss", test_loss, "Final test variance", test_variance)
+    print("MSE/Variance ratio ", (test_loss/test_variance))
+
+    # Write the entire series to a file, which can be used to later construct the graph
+    out = DataFrame(shifted_dataframe_narray)
+    out['1'] = test_plot
+    out.to_csv('orig_prediction.csv',sep=',',encoding='utf-8')
 
 
-# In[31]:
-
-print("Final test loss", test_loss, "Final test variance", test_variance)
-print("MSE/Variance ratio ", (test_loss/test_variance))
-#plt.plot(shifted_dataframe_narray)
-#plt.plot(test_plot)
-#plt.show()
-#plt.savefig('plot.png')
-
-out = DataFrame(shifted_dataframe_narray)
-out['1'] = test_plot
-out.to_csv('orig_prediction.csv',sep=',',encoding='utf-8')
-#print("Output", out)
+if __name__ == '__main__':
+    filename = '/home/rgangaraju/chaos/entropy_data/xalan-smallJikesRVM-both-100k-64.entropy'
+    train(filename)
 
 
