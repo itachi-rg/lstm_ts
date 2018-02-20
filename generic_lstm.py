0dc036bf4979b4ef7c2fc79deaf5c0ad6e28a3bf
itachi-rg <rohan.gangaraju@gmail.com>
2018-02-20 14:42:25 -0500

    pushing results for all mse

--- generic_lstm.py
+++ generic_lstm.py
@@ -30,15 +30,16 @@ def getFileVector(absolute_path):
     data_inst_vector = splitnames[-3]
     benchmark_vector = ''.join(splitnames[1:-3])
     prog_vector = splitnames[0]
-    print(filename)
+    print("filename ", filename)
     print(prog_vector,benchmark_vector,data_inst_vector,block_size_vector,cpu_arch_vector)
     vector_string += getbin(prog[prog_vector], 5)
     vector_string += getbin(benchmark[benchmark_vector], 6)
     vector_string += getbin(data_inst[data_inst_vector],2)
     vector_string += getbin(block_size[block_size_vector],1)
     vector_string += getbin(cpu_arch[cpu_arch_vector],1)
-    print(vector_string)
+    print("vector string ",vector_string)
     vector = [int(bit) for bit in list(vector_string)]
+    print("vector ", vector)
     return vector
 
 
@@ -51,9 +52,10 @@ def train(training_file, iterations=5000, time_steps=50, num_lstm_hidden_units=1
     series = read_csv(training_file)
 
     file_vector = getFileVector(training_file)
-    file_vector_size = len(file_vector)
+    #file_vector_size = len(file_vector)
 
     print("File vector ", file_vector)
+    file_vector_size = len(file_vector)
     
     # Convert series to dataframe to perform shift and generate test set
     series_dataframe = DataFrame(series)
@@ -65,30 +67,33 @@ def train(training_file, iterations=5000, time_steps=50, num_lstm_hidden_units=1
     shifted_dataframe_narray = shifted_dataframe.as_matrix()
 
     # Create small samples of time_steps size
-    split_length = file_vector_size+time_steps
+    split_length = time_steps
     num_samples = shifted_dataframe_narray.shape[0]-split_length
 
     file_vector_narray = np.array(file_vector).reshape(1,1,-1)
 
-    print("File vector reshaped before np repeat ", file_vector_narray.shape)
+    #print("File vector reshaped before np repeat ", file_vector_narray.shape)
 
     file_vector_repeat = np.repeat(np.repeat(file_vector_narray,num_samples,axis=0),split_length,axis=1)
 
-    print("X_samples shape repeated ", X_samples.shape)
+    #print("X_samples shape repeated ", file_vector_repeat.shape)
+    num_features = file_vector_size+1
 
-    X_samples = np.zeros([num_samples, split_length, file_vector_size+1])
+    X_samples = np.zeros([num_samples, split_length, num_features])
     y_samples = np.zeros([num_samples, 1])
 
-    print("X_samples shape ", X_samples.shape)
+    #print("X_samples shape ", X_samples.shape)
 
     for i in range(num_samples) :
-        X_samples[i] = np.append(file_vector_repeat[i],shifted_dataframe_narray[i:i+split_length,0],axis=1)
+        #print("X samples shape ", X_samples[i].shape)
+        #print("shifted ", shifted_dataframe_narray[i:i+split_length,0].reshape(-1,1).shape)
+        X_samples[i] = np.append(file_vector_repeat[i],shifted_dataframe_narray[i:i+split_length,0].reshape(-1,1),axis=1)
 
-        print("X_samples ", X_samples[i])
+        #print("X_samples ", X_samples[i])
         y_samples[i] = series_dataframe_narray[i+split_length,0]
 
     # Add additional dimension to feed it into tensorflow. It needs 3D tensor for LSTM
-    X_samples = np.reshape(X_samples,[X_samples.shape[0],X_samples.shape[1],1])
+    #X_samples = np.reshape(X_samples,[X_samples.shape[0],X_samples.shape[1],1])
 
 
     # Split into training and test data
@@ -192,34 +197,6 @@ def train(training_file, iterations=5000, time_steps=50, num_lstm_hidden_units=1
     #out.to_csv('orig_prediction.csv',sep=',',encoding='utf-8')
     return test_loss, test_variance
 
-def getbin(value, width):
-    return format(value, "{0:0"+str(width)+"b}")
-
-def getFileVector(absolute_path):
-    cpu_arch = {'4096':0, '64':1}
-    block_size = {'1m':0, '100k':1}
-    data_inst =  {'inst':0, 'both':1, 'data':2}
-    benchmark = {'refequake': 0, 'refmcf': 1, 'refbzip23': 2, 'refapsi': 3, 'refcc15': 4, 'defaultJ9': 5, 'refparser': 6, 'refgalgel': 7, 'refapplu': 8, 'refperlbmk2': 9, 'tinyJikesRVM': 10, 'refbzip22': 11, 'defaultJikesRVM': 12, 'refcrafty': 13, 'defaultHotSpot': 14, 'refvpr2': 15, 'smallJikesRVM': 16, 'refcc12': 17, 'refperlbmk1': 18, 'smallHotSpot': 19, 'smallJ9': 20, 'small6trace': 21, 'refammp': 22, 'refvpr1': 23, 'refart': 24, 'small1trace': 25, 'small0trace': 26, 'tinyHotSpot': 27, 'refvortex': 28, 'refperlbmk5': 29, 'refvortex3': 30, 'refmesa': 31, 'refbzip21': 32, 'refperlbmk6': 33, 'refperlbmk3': 34, 'small3trace': 35}
-    prog = {'parser': 0, 'luindex': 1, 'lusearch': 2, 'fop': 3, 'batik': 4, 'gcc': 5, 'equake': 6, 'galgel': 7, 'mesa': 8, 'pmd': 9, 'vortex': 10, 'apsi': 11, 'tomcat': 12, 'crafty': 13, 'vpr': 14, 'h2': 15, 'xalan': 16, 'avrora': 17, 'applu': 18, 'art': 19, 'sunflow': 20, 'bzip2': 21, 'ammp': 22, 'perlbmk': 23, 'jython': 24, 'eclipse': 25, 'mcf': 26}
-    filename_w_extn = absolute_path.split('/')[-1]
-    filename = filename_w_extn.split('.')[0]
-    splitnames = filename.split('-')
-    vector_string = ""
-    cpu_arch_vector = splitnames[-1]
-    block_size_vector = splitnames[-2]
-    data_inst_vector = splitnames[-3]
-    benchmark_vector = ''.join(splitnames[1:-3])
-    prog_vector = splitnames[0]
-    print(filename)
-    print(prog_vector,benchmark_vector,data_inst_vector,block_size_vector,cpu_arch_vector)
-    vector_string += ' ' + getbin(prog[prog_vector], 5)
-    vector_string += ' ' + getbin(benchmark[benchmark_vector], 6)
-    vector_string += ' ' + getbin(data_inst[data_inst_vector],2)
-    vector_string += ' ' + getbin(block_size[block_size_vector],1)
-    vector_string += ' ' + getbin(cpu_arch[cpu_arch_vector],1)
-    print(vector_string)
-
-
 if __name__ == '__main__':
     filename = '/home/rgangaraju/chaos/entropy_data/xalan-smallJikesRVM-both-100k-64.entropy'
     filedir = '/home/rgangaraju/chaos/selectedFiles_rishikesh/files'
@@ -230,7 +207,7 @@ if __name__ == '__main__':
     for data_file in files:
         tf.reset_default_graph()
         print("File : ", data_file)
-        test_loss, test_variance = train(data_file, iterations=200, learning_rate=0.01,batch_size=65536,print_iter=50)
+        test_loss, test_variance = train(data_file, iterations=200, learning_rate=0.01,batch_size=16384,print_iter=50)
         result_dict[data_file] = test_loss/test_variance
         print("==================================================================")
 
